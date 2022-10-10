d68973fbd7d0fce11ba5e1070a628829e6a30987
yaoyuanrong <yaoyuanrong@soyoung.com>
2022-10-10 17:02:35 +0800

    改进

--- app/renderer/pages/control/app.js
+++ app/renderer/pages/control/app.js
@@ -11,7 +11,16 @@ function play(stream) {
     video.play()
   }
 }
-
+function sreenData (e) {
+  let data = {}
+  data.clientX = e.clientX
+  data.clientY = e.clientY
+  data.video = {
+    width: video.getBoundingClientRect().width,
+    height: video.getBoundingClientRect().height
+  }
+  return data
+}
 window.onkeydown = function (e) {
   console.log(e)
   let data = {
@@ -23,24 +32,19 @@ window.onkeydown = function (e) {
   }
   peer.emit('robot', 'key', data)
 }
-
 window.onmouseup = function (e) {
-  let data = {}
-  data.clientX = e.clientX
-  data.clientY = e.clientY
-  data.video = {
-    width: video.getBoundingClientRect().width,
-    height: video.getBoundingClientRect().height
-  }
+  let data = sreenData(e)
   peer.emit('robot', 'mouse',data)
 }
+window.onmousedown = function (e) {
+  let data = sreenData(e)
+  peer.emit('robot', 'mouseDown',data)
+} 
 window.onmousemove = function (e) {
-  let data = {}
-  data.clientX = e.clientX
-  data.clientY = e.clientY
-  data.video = {
-    width: video.getBoundingClientRect().width,
-    height: video.getBoundingClientRect().height
-  }
+  let data = sreenData(e)
   peer.emit('robot', 'mouseMove',data)
+}
+window.oncontextmenu = function (e) {
+  let data = sreenData(e)
+  peer.emit('robot', 'clickRight',data)
 }
