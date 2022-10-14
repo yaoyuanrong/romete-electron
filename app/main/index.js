b16000bd9ea062f37ddc9844ba997a36b95c025f
yaoyuanrong <yaoyuanrong@soyoung.com>
2022-10-14 11:27:49 +0800

    123

--- app/main/index.js
+++ app/main/index.js
@@ -25,6 +25,8 @@ const gotTheLock = app.requestSingleInstanceLock()
 		app.on('before-quit', () => {
 			signal.send('control-quit-fresh')
 			console.log('sendMainWindow','control-quit-fresh')
+		})
+		app.on('quit', () => {
 			closeMainWindow()
 		})
 		app.on('activate', () => {
